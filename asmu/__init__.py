d654097b3a87f72afc3f11d50db2258593351793
felhub <felhub@net4us.at>
2024-04-05 14:47:31 +0200

    absoulte import

--- asmu/__init__.py
+++ asmu/__init__.py
@@ -1,9 +1,9 @@
-from .analysis import get_corrs_sampleshifts, calSPL
-from .generator import SincBurst, Sine, WhiteNoise, SineBurst, Chirp
-from .inout import Input, Output
-from .interface import Interface
-from .recording import Recording
-from .setup import Setup
+from asmu.analysis import get_corrs_sampleshifts, calSPL
+from asmu.generator import SincBurst, Sine, WhiteNoise, SineBurst, Chirp
+from asmu.inout import Input, Output
+from asmu.interface import Interface
+from asmu.recording import Recording
+from asmu.setup import Setup
 #NOT IMPLEMENTED from .view import TimeView, Corrview
 
 __all__ = ["get_corrs_sampleshifts", "calSPL", "SincBurst", "Sine", "WhiteNoise", "SineBurst", "Chirp", "generator", "Input", "Output", "Interface", "Recording", "Setup"]
