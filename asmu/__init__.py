aab3e135d4baa42fbe34900e1653159468448740
felhub <felhub@net4us.at>
2024-04-09 12:23:05 +0200

    new names added

--- asmu/__init__.py
+++ asmu/__init__.py
@@ -1,9 +1,14 @@
-from asmu.analysis import get_corrs_sampleshifts, calSPL
-from asmu.generator import SincBurst, Sine, WhiteNoise, SineBurst, Chirp
+from asmu.analyzer import get_corrs_sampleshifts, calSPL, Recorder
+from asmu.generator import SincBurst, Sine, WhiteNoise, SineBurst, Chirp, Player
 from asmu.inout import Input, Output
 from asmu.interface import Interface
-from asmu.recording import Recording
-from asmu.setup import Setup
+from asmu.afile import AFile
+from asmu.asetup import ASetup
 #NOT IMPLEMENTED from .view import TimeView, Corrview
 
-__all__ = ["get_corrs_sampleshifts", "calSPL", "SincBurst", "Sine", "WhiteNoise", "SineBurst", "Chirp", "generator", "Input", "Output", "Interface", "Recording", "Setup"]
+__all__ = ["get_corrs_sampleshifts", "calSPL", "Recorder",
+           "SincBurst", "Sine", "WhiteNoise", "SineBurst", "Chirp", "Player",
+           "Input", "Output",
+           "Interface",
+           "AFile",
+           "ASetup"]
